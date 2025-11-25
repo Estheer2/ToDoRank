51916e63ce26127116a42729bf75d2e13c5595b3
Estheer2 <113109316+Estheer2@users.noreply.github.com>
2025-11-25 10:47:47 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -68,8 +68,7 @@ app.get("/rank", (req, res) => {
 
 /* ---------------------- START SERVER ---------------------- */
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, "0.0.0.0", () => {
-    console.log("Servidor rodando na porta " + PORT);
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
 });
+
